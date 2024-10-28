0d50e4372b2291c00126877a0f7d9be10dc24175
Subways-Sun <subways-sun@hotmail.com>
2024-10-28 20:58:46 +0000

    Change of directory structure

--- scripts/search_for_relevant_papers.py
+++ scripts/search_for_relevant_papers.py
@@ -1,13 +1,13 @@
 """Script to retrive relevant paper"""
 
 import json
-from src import semantic_scholar as ss
+import sib_package.literature_retrival.semantic_scholar as ss
 
 WORK_DIR = r"%HOMEPATH%\OneDrive\Documents\GitHub\sodium-ion-batteries\literature-retrival"
 
-query = "sodium+ion+battery"
+QUERY = "sodium+ion+battery+anode"
 
-result = ss.search(query, limit = 100, fields = "title,abstract")
+result = ss.search(QUERY, limit = 100, fields = "title,abstract")
 RESULT_PATH = WORK_DIR + r"\data\search.json"
 with open(RESULT_PATH, "w", encoding='utf-8') as f:
     json.dump(result, f, ensure_ascii=False, indent=4)
