704fcdfbd2c030bb0b0bc73ededc190a4d4456f8
Subways-Sun <subways-sun@hotmail.com>
2024-10-28 20:01:03 +0000

    Refactor search query in literature retrieval script for clarity

--- literature-retrival/scripts/search_for_relevant_papers.py
+++ literature-retrival/scripts/search_for_relevant_papers.py
@@ -5,7 +5,9 @@ from src import semantic_scholar as ss
 
 WORK_DIR = r"%HOMEPATH%\OneDrive\Documents\GitHub\sodium-ion-batteries\literature-retrival"
 
-result = ss.search("sodium+ion+battery", limit = 100, fields = "title,abstract")
+query = "sodium+ion+battery"
+
+result = ss.search(query, limit = 100, fields = "title,abstract")
 RESULT_PATH = WORK_DIR + r"\data\search.json"
 with open(RESULT_PATH, "w", encoding='utf-8') as f:
     json.dump(result, f, ensure_ascii=False, indent=4)
