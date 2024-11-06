f54f0b6303b3b4dcd213682b2eba2656fb53f200
Subways-Sun <subways-sun@hotmail.com>
2024-11-06 23:33:06 +0000

    Add capability to remove entries with no abstract, change remove review articles to only include journal articles

--- sib/literature/lit_processing.py
+++ sib/literature/lit_processing.py
@@ -19,15 +19,28 @@ def remove_duplicates(json_file_path):
     with open(json_file_path, 'w', encoding='utf-8') as file:
         json.dump(unique_entries, file, indent=4)
 
-def remove_reviews(json_file_path):
-    """Function to remove review articles from a JSON file"""
+def keep_journal(json_file_path):
+    """Function to only retain journal articles in a JSON file"""
     with open(json_file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    no_reviews = []
+    journal = []
     for entry in data:
-        if 'Review' not in entry.get('publicationTypes'):
-            no_reviews.append(entry)
+        if entry.get('publicationTypes') == ['JournalArticle']:
+            journal.append(entry)
 
     with open(json_file_path, 'w', encoding='utf-8') as file:
-        json.dump(no_reviews, file, ensure_ascii=False, indent=4)
+        json.dump(journal, file, ensure_ascii=False, indent=4)
+
+def remove_no_abstract(json_file_path):
+    """Function to remove papers with no abstract from a JSON file"""
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    with_abstract = []
+    for entry in data:
+        if entry.get('abstract'):
+            with_abstract.append(entry)
+
+    with open(json_file_path, 'w', encoding='utf-8') as file:
+        json.dump(with_abstract, file, ensure_ascii=False, indent=4)
