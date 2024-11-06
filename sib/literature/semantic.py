da410e287afa086d8770b6b52685146fcf6e15d9
Subways-Sun <subways-sun@hotmail.com>
2024-11-06 22:18:45 +0000

    When 429 is returned, automatically try again

--- sib/literature/semantic.py
+++ sib/literature/semantic.py
@@ -1,13 +1,14 @@
-"""Module performing API calls to Semantic Scholar"""
+"""Module performing API calls to Semantic Scholar."""
 
-# Include DOI
-# Publication data
-# Different query and merge result
-# No review papers
-# Restrict publisher
-# BatteryBERT
-# Bulk Search
+# OK Include DOI
+# OK Publication data
+# WIP Different query and merge result
+# WIP No review papers
+# WIP Restrict publisher
+# WIP BatteryBERT
+# OK Bulk Search
 
+import time
 import requests
 
 def search(query, **kwargs):
@@ -27,11 +28,11 @@ def search(query, **kwargs):
 
     if limit is not None:
         l = "&limit=" + str(limit)
-    if fields is not None:
+    if fields is not (None or ""):
         f = "&fields=" + fields
     if offset is not None:
         o = "&offset=" + str(offset)
-    if publication_types is not None:
+    if publication_types is not (None or ""):
         pt = "&publicationTypes=" + publication_types
 
     url = f"{base_url}{q}{o}{l}{f}{pt}"
@@ -41,12 +42,13 @@ def search(query, **kwargs):
 
     if response.status_code == 200:
         result = response.json()['data']
-    elif response.status_code == 429:
-        result = response.json()['message']
     elif response.status_code == 400:
-        result = response.json()['error']
+        result = [response.json()]
+    elif response.status_code == 429:
+        time.sleep(2)
+        return search(query, **kwargs)
     else:
-        result = response.status_code
+        result = [response.status_code]
     return [result, url]
 
 def search_bulk(query, **kwargs):
@@ -62,11 +64,11 @@ def search_bulk(query, **kwargs):
     pt = ""
     t = ""
 
-    if fields is not None:
+    if fields is not (None or ""):
         f = "&fields=" + fields
-    if publication_types is not None:
+    if publication_types is not (None or ""):
         pt = "&publicationTypes=" + publication_types
-    if token is not None:
+    if token is not (None or ""):
         t = "&token=" + token
 
     url = f"{base_url}{q}{f}{pt}{t}"
@@ -78,10 +80,11 @@ def search_bulk(query, **kwargs):
     if response.status_code == 200:
         result = response.json()['data']
         result_token = response.json()['token']
-    elif response.status_code == 429:
-        result = response.json()['message']
     elif response.status_code == 400:
-        result = response.json()['error']
+        result = [response.json()]
+    elif response.status_code == 429:
+        time.sleep(2)
+        return search_bulk(query, **kwargs)
     else:
-        result = response.status_code
+        result = [response.status_code]
     return [result, url, result_token]
