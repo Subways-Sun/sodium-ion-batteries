4999c20262c00f3de5724c38771543c9fb102c12
Subways-Sun <subways-sun@hotmail.com>
2024-11-06 23:33:55 +0000

    Update script to take advantage of new module capabilities, fetch new data

--- scripts/search_for_relevant_papers.py
+++ scripts/search_for_relevant_papers.py
@@ -2,9 +2,10 @@
 
 import json
 import platform
-import time
+from datetime import datetime
 from pathlib import Path
 import sib.literature.semantic as ss
+import sib.literature.lit_processing as lp
 
 # Set working directory based on the operating system
 HOME_PATH = str(Path.home())
@@ -15,24 +16,48 @@ if platform.system() == "Darwin":
     WORK_DIR = HOME_PATH + r"/Documents/GitHub/sodium-ion-batteries"
 
 # Define search parameters
-QRY = "sodium+ion+battery+anode"
-LMT = 100
+QRYLST = ["sodium+ion+battery+anode",
+          "sodium+ion+battery+cathode",
+          "sodium+ion+battery+electrode"]
+
 FLD = "externalIds,title,abstract,publicationTypes,publicationDate,venue"
-OFSLST = range(0, 500, 100)
+CNT = 100 # Number of papers to retrieve
+
+LMT = 100
+OFSLST = range(0, CNT, 100)
+PBL = "JournalArticle"
+BLK_PASS = 1
+BLK_TOKEN = ""
 
 result = list([])
 # Search for relevant papers
-for OFS in OFSLST:
-    temp = ss.search(QRY, offset = OFS, limit = LMT, fields = FLD)[0]
-    for item in temp:
-        result.append(item)
-    time.sleep(3)
+for QRY in QRYLST:
+    for OFS in OFSLST:
+        temp = ss.search(QRY, offset = OFS, limit = LMT, fields = FLD, publicationTypes = PBL)[0]
+        for item in temp:
+            result.append(item)
+
+# for QRY in QRYLST:
+#     temp = ss.search_bulk(QRY, fields = FLD, publicationTypes = PBL)[0]
+#     for item in temp:
+#         result.append(item)
 
 # Save the result to a json file
 RESULT_PATH = ""
+current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
 if platform.system() == "Windows":
-    RESULT_PATH = WORK_DIR + rf"\data\search_{QRY}_{len(result)}.json"
+    SEARCH_NAME = "-".join(QRYLST)
+    RESULT_PATH = WORK_DIR + rf"\data\search_{current_time}_{SEARCH_NAME}.json"
 if platform.system() == "Darwin":
-    RESULT_PATH = WORK_DIR + rf"/data/search_{QRY}_{len(result)}.json"
-with open(RESULT_PATH, "w", encoding='utf-8') as f:
-    json.dump(result, f, ensure_ascii=False, indent=4)
+    RESULT_PATH = WORK_DIR + rf"/data/search_{current_time}_{SEARCH_NAME}.json"
+with open(RESULT_PATH, "w", encoding='utf-8') as file:
+    json.dump(result, file, ensure_ascii=False, indent=4)
+
+# Remove duplicates
+lp.remove_duplicates(RESULT_PATH)
+
+# Only keep journal articles
+lp.keep_journal(RESULT_PATH)
+
+# Remove papers with no abstract
+lp.remove_no_abstract(RESULT_PATH)
