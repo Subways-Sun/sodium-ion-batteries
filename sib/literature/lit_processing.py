e27c2c8913f02eb65fa766da1ef27b062bc0abba
Subways-Sun <subways-sun@hotmail.com>
2025-01-23 00:18:41 +0000

    Added functionality to keep relevant papers & papers from select publishers

--- sib/literature/lit_processing.py
+++ sib/literature/lit_processing.py
@@ -56,3 +56,29 @@ def remove_no_abstract(json_file_path):
 
     with open(json_file_path, 'w', encoding='utf-8') as file:
         json.dump(with_abstract, file, ensure_ascii=False, indent=4)
+
+def keep_select_publishers(json_file_path, publishers: list):
+    """Function to only retain papers from select publishers in a JSON file"""
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    selected_list = []
+    for entry in data:
+        if entry.get('externalIds').get('DOI').split('/')[0] in publishers:
+            selected_list.append(entry)
+    
+    with open(json_file_path, 'w', encoding='utf-8') as file:
+        json.dump(selected_list, file, ensure_ascii=False, indent=4)
+
+def keep_relevant(json_file_path, label: str):
+    """Function to only retain relevant papers in a labelled JSON file"""
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    relevant_list = []
+    for entry in data:
+        if entry.get(label) == 1:
+            relevant_list.append(entry)
+
+    with open(json_file_path, 'w', encoding='utf-8') as file:
+        json.dump(relevant_list, file, ensure_ascii=False, indent=4)
