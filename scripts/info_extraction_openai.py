912a08cebd4920bb98977a029148bb788b97bca3
Subways-Sun <subways-sun@hotmail.com>
2025-04-27 20:25:33 +0800

    Update script

--- scripts/info_extraction_openai.py
+++ scripts/info_extraction_openai.py
@@ -4,6 +4,8 @@ import json
 import os
 import logging
 import time
+
+import openai
 # import tiktoken
 from sib.literature.openai import extract
 from sib.literature.lit_processing import read_json, write_json, extract_content
@@ -19,6 +21,7 @@ logging.basicConfig(
 )
 
 openai_model = "o3-mini"
+reasoning_effort = "medium"
 
 full_text_dir = os.path.join(os.getcwd(), "extraction")
 logging.debug(f"Full text directory: {full_text_dir}")
@@ -29,19 +32,25 @@ for file in os.listdir(full_text_dir):
         full_text = read_json(file_path)
         logging.debug(f"Reading file: {file_path}")
 
-        results = extract_content(full_text, "Result")
-        if results == "":
-            logging.warning(f"Result section not found in {file}, skipping...")
-            continue
+        results = extract_content(full_text, "")
+        # if results == "":
+        #     logging.warning(f"Result section not found in {file}, using full text...")
+        #     results = extract_content(full_text, "")
         experimental = extract_content(full_text, "Experiment")
         if experimental == "":
-            logging.warning(f"Experimental section not found in {file}")
+            experimental = extract_content(full_text, "Method")
+            if experimental == "":
+                logging.warning(f"Experimental section not found in {file}")
 
-        datas = extract(openai_model, results, "results")
-        data = json.loads(datas)
+        datas = extract(openai_model, results, "results", "", reasoning_effort)
+        logging.info(f"OpenAI Response: {datas[1]}")
+        data = json.loads(datas[0])
         if experimental != "":
             for i in data["material"]:
-                sm = extract(openai_model, experimental, "experimental", i["material_name"])
-                i["starting_material"] = json.loads(sm)["starting_material"]
-
-        write_json(os.path.join(os.getcwd(), "extracted", f"{file.removesuffix('.json')}_processed_{openai_model}.json"), data)
+                sm = extract(openai_model, experimental, "experimental", i["material_name"], reasoning_effort=reasoning_effort)
+                i["starting_material"] = json.loads(sm[0])["starting_material"]
+                logging.info(f"OpenAI Response: {sm[1]}")
+        if openai_model == "o3-mini": 
+            write_json(os.path.join(os.getcwd(), "extracted_2", f"{file.removesuffix('.json')}_processed_{openai_model}_{reasoning_effort}.json"), data)
+        if openai_model == "gpt-4o":
+            write_json(os.path.join(os.getcwd(), "extracted_2", f"{file.removesuffix('.json')}_processed_{openai_model}.json"), data)
